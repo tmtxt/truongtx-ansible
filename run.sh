fee2ea8aa8c614e637967fe5dee477d9f4229cb1
TruongTX <me@truongtx.me>
2018-08-22 19:32:47 +0700

    Generate password for personal user

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-# ansible-playbook -K -i hosts --extra-vars "@password.yml" --tags=bootstrap vps.yml
-ansible-playbook -K -i hosts --tags=bootstrap vps.yml
+ansible-playbook -i hosts --ask-vault-pass main.yml
