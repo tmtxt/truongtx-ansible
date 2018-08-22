b6fe2c6163b9cd81b776ed7ce09461f14ada50f7
TruongTX <me@truongtx.me>
2018-08-22 17:24:43 +0700

    Add new files for personal hosts

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-ansible-playbook -K -i hosts --extra-vars "@password.yml" vps.yml
+# ansible-playbook -K -i hosts --extra-vars "@password.yml" --tags=bootstrap vps.yml
+ansible-playbook -K -i hosts --tags=bootstrap vps.yml
