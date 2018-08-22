52bb9c5798e77a129d86e07419715524aa770630
TruongTX <me@truongtx.me>
2018-08-22 20:03:25 +0700

    Add playbook for setup tmtxt

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-ansible-playbook -i hosts --ask-vault-pass main.yml
+# ansible-playbook -i hosts --ask-vault-pass main.yml
+ansible-playbook -i hosts --ask-vault-pass --tags "setup-tmtxt" main.yml
